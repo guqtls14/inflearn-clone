64895e5178fad47332ca4fc83e29e5670b061cef
kyle-shk <guqtls85@naver.com>
2022-04-20 18:41:42 +0900

    feat: add review + banner slider

--- main.js
+++ main.js
@@ -15,20 +15,82 @@ $btn.addEventListener('click',function(){
 })
 
 // swiper
-let swiper = new Swiper(".mySwiper", {
+let swiper1 = new Swiper(".mySwiper", {
     slidesPerView:5,
     // spaceBetween:10,
     centeredSlides: true,
     loop:true,
     autoplay:{
-        delay:2000,
+        delay:4000,
     },
-    pagination: {
-        el: ".swiper-pagination",
-        type: "fraction"
-      },
+    // pagination: {
+    //     el: ".swiper-pagination",
+    //     type: "fraction"
+    //   },
+    // navigation: {
+    //   nextEl: ".swiper-button-next",
+    //   prevEl: ".swiper-button-prev",
+    // },
+  });
+
+//   roadmap
+var swiper2 = new Swiper(".roadmap .swiper", {
+    slidesPerView:4,
+    spaceBetween:65,
+    centeredSlides: true,
+    loop:true,
+    autoplay:{
+        delay:3000,
+    },
+
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+      nextEl: ".swiper--btn .swiper-button-next",
+      prevEl: ".swiper--btn .swiper-button-prev",
     },
   });
+
+//  new lecture
+var swiper3 = new Swiper(".newLecture .swiper", {
+    slidesPerView:"auto",
+    spaceBetween:10,
+    centeredSlides: true,
+    loop:true,
+    autoplay:{
+        delay:2000,
+    },
+
+    navigation: {
+      nextEl: ".swiper--btn .swiper-button-next",
+      prevEl: ".swiper--btn .swiper-button-prev",
+    },
+  });
+// review
+let swiper4 = new Swiper(".review .swiper",{
+  direction: "vertical",
+  slidesPerView:4,
+  spaceBetween:10,
+  loop:true,
+  autoplay:{
+    delay:3000,
+},
+})
+
+  // banner2
+  let swiper5 = new Swiper(".banner2 .swiper",{
+    slidesPerView:1,
+    spaceBetween:10,
+    centeredSlides: true,
+    // loop:true,
+    // autoplay:{
+    //     delay:2000,
+    // },
+
+    navigation: {
+      nextEl: ".banner2 .btn--container .swiper-button-next",
+      prevEl: ".banner2 .btn--container .swiper-button-prev",
+    },
+    pagination: {
+      el: ".banner2 .swiper-pagination",
+      clickable: true,
+    },
+  })
